a18c9ea38e9931cabbadb53e000b55fe524b64fa
Shikhar2929 <ahujashikhar314@gmail.com>
2024-10-26 16:22:47 -0400

    Java Environment Setup

---
+++ src/main/java/HighThroughPutExchange/MatchingEngine/MatchingEngine.java
@@ -0,0 +1,22 @@
+package HighThroughPutExchange.MatchingEngine;
+
+public class MatchingEngine {
+    MatchingEngine(){}
+    private long orderID = 0;
+
+    public double getHighestBid() {
+
+    }
+    public double getLowestAsk() {
+
+    }
+    public long bidLimitOrder(String name, Order order) {
+
+    }
+    public long askLimitOrder(String name, Order order) {
+
+    }
+    public void display() {
+
+    }
+}
