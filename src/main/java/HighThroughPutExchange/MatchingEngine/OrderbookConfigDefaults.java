6910923e4cd94e4bc0dd998bd9ff73abd1270700
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2024-12-16 03:52:59 +0530

    pushing for better JSON

---
+++ src/main/java/HighThroughPutExchange/MatchingEngine/OrderbookConfigDefaults.java
@@ -0,0 +1,35 @@
+package HighThroughPutExchange.MatchingEngine;
+
+import java.util.HashMap;
+
+public class OrderbookConfigDefaults {
+    private String[] tickers;
+    private HashMap<String, Integer> balances;
+    private int defaultBalance;
+
+    public OrderbookConfigDefaults() {}
+
+    public String[] getTickers() {
+        return tickers;
+    }
+
+    public void setTickers(String[] tickers) {
+        this.tickers = tickers;
+    }
+
+    public HashMap<String, Integer> getBalances() {
+        return balances;
+    }
+
+    public void setBalances(HashMap<String, Integer> balances) {
+        this.balances = balances;
+    }
+
+    public int getDefaultBalance() {
+        return defaultBalance;
+    }
+
+    public void setDefaultBalance(int defaultBalance) {
+        this.defaultBalance = defaultBalance;
+    }
+}
