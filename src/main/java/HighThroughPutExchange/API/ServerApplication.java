8099b480552a158d66cc0066d52e3a4f565897bb
idkwhttsay <danil.tatarinov.00@gmail.com>
2026-01-11 13:05:08 -0500

    refac: spotless:apply

--- src/main/java/HighThroughPutExchange/API/ServerApplication.java
+++ src/main/java/HighThroughPutExchange/API/ServerApplication.java
@@ -1,6 +1,5 @@
 package HighThroughPutExchange.API;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +30,6 @@ public class ServerApplication {
     }
 
     // public static void main(String[] args) {
-    //     SpringApplication.run(ServerApplication.class, args);
+    // SpringApplication.run(ServerApplication.class, args);
     // }
 }
