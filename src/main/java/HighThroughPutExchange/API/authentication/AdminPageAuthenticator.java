d1933aed3e817c3a7a9f0cf7cc29c5c65a412f28
idkwhttsay <danil.tatarinov.00@gmail.com>
2026-01-11 15:59:38 -0500

    feat: add .env

--- src/main/java/HighThroughPutExchange/API/authentication/AdminPageAuthenticator.java
+++ src/main/java/HighThroughPutExchange/API/authentication/AdminPageAuthenticator.java
@@ -1,13 +1,18 @@
 package HighThroughPutExchange.API.authentication;
 
 import HighThroughPutExchange.API.api_objects.requests.BaseAdminRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminPageAuthenticator {
-    // todo read values from env variables
-    private final String adminUsername = "trading_club_admin";
-    private final String adminPassword = "ZY3yoQL5v8MahcmcWBnG";
+    private final String adminUsername;
+    private final String adminPassword;
+
+    public AdminPageAuthenticator(@Value("${admin.username}") String adminUsername, @Value("${admin.password}") String adminPassword) {
+        this.adminUsername = adminUsername;
+        this.adminPassword = adminPassword;
+    }
 
     public boolean authenticate(BaseAdminRequest req) {
         return req.getAdminUsername().equals(adminUsername) && req.getAdminPassword().equals(adminPassword);
