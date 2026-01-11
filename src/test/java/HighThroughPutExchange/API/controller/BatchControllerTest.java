e41f8bbd776644272a1834234e24e7557befca10
Daniil Tatarinov <92638932+idkwhttsay@users.noreply.github.com>
2026-01-11 13:18:35 -0500

    [SERVICE/CONTROLLER REFAC] Tests for Controllers (#60)
    * refac: OrderService
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: OrderController
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: Auth Service + Controller
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: Session Service + Controller
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: turn LocalDBTables into injectable beans
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * fix: unnecessary casts and warnings in LocalDBTable
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * Revert "fix: unnecessary casts and warnings in LocalDBTable"
    
    This reverts commit 30c4c20ff071af0a05160b43bc9f12127cc33524.
    
    * refac: Auction Service + Controller
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: Admin Service + Controller
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: miscellaneous services and controllers
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * clean: spotless
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * feat: add endpoint tests
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * refac: reformat mock request bodies
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>
    
    * fix: order controller test based on auth changes
    
    * spotless
    
    ---------
    
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

---
+++ src/test/java/HighThroughPutExchange/API/controller/BatchControllerTest.java
@@ -0,0 +1,58 @@
+package HighThroughPutExchange.API.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import HighThroughPutExchange.API.ServerApplication;
+import HighThroughPutExchange.API.State;
+import HighThroughPutExchange.API.api_objects.responses.OperationResponse;
+import HighThroughPutExchange.API.service.AuthService;
+import HighThroughPutExchange.API.service.BatchService;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = BatchController.class)
+@AutoConfigureMockMvc
+class BatchControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BatchService batchService;
+
+    @MockBean
+    private AuthService authService;
+
+    @MockBean
+    private ServerApplication app;
+
+  @Test
+  void batch_success_empty() throws Exception {
+    when(authService.authenticateBot(any())).thenReturn(true);
+    when(app.getState()).thenReturn(State.TRADE);
+    when(batchService.getMaxOperations()).thenReturn(20);
+    when(batchService.processBatch(Mockito.eq("bot1"), Mockito.anyList()))
+        .thenReturn(new ArrayList<OperationResponse>());
+
+    String body =
+        """
+        {
+            "username": "bot1",
+            "sessionToken": "t",
+            "operations": []
+        }
+        """;
+    mockMvc
+        .perform(post("/batch").contentType(MediaType.APPLICATION_JSON).content(body))
+        .andExpect(status().isOk());
+  }
+}
---
+++ src/test/java/HighThroughPutExchange/API/controller/SystemControllerTest.java
@@ -0,0 +1,57 @@
+package HighThroughPutExchange.API.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import HighThroughPutExchange.API.ServerApplication;
+import HighThroughPutExchange.API.authentication.RateLimiter;
+import HighThroughPutExchange.API.service.AuthService;
+import HighThroughPutExchange.API.service.SystemService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = SystemController.class)
+@AutoConfigureMockMvc
+class SystemControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @MockBean
+    private SystemService systemService;
+
+    @MockBean
+    private RateLimiter rateLimiter;
+
+    @MockBean
+    private ServerApplication app;
+
+  @Test
+  void getDetails_success() throws Exception {
+    when(authService.authenticatePrivate(any())).thenReturn(true);
+    when(rateLimiter.processRequest(any())).thenReturn(true);
+    when(systemService.getUserDetails("trader")).thenReturn("DETAILS");
+
+    String body =
+        """
+        {
+          "username": "trader",
+          "sessionToken": "tok"
+        }
+        """;
+    mockMvc
+        .perform(post("/get_details").contentType(MediaType.APPLICATION_JSON).content(body))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.userDetails").value("DETAILS"));
+  }
+}
